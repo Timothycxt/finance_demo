f1b799f101ec49b690acac1faa8d7704c59bd6ed
GuangJun <3318882056@qq.com>
2020-12-21 19:22:05 +0800

    apis/economic_new.py代码错误

--- App/apis/economic_news.py
+++ App/apis/economic_news.py
@@ -1,15 +1,15 @@
 from flask import Blueprint
 from flask import jsonify
-from App.models.indu_news import Indu_news
+from App.models.economic_news import EconomicNews
 
-indu_news = Blueprint('indu_news', __name__)
+economic_new = Blueprint('economic_news', __name__)
 
-@indu_news.route('/indu_news',methods=['GET'])
+@economic_new.route('/economic_news',methods=['GET'])
 def economic_news():
-    indu_news=Indu_news.query.all()
+    economic_news=EconomicNews.query.all()
     data = []
     res = {}  # 返回一个字典，包含状态码，信息，数据
-    for item in indu_news:
+    for item in economic_news:
         data.append(item.to_json())
     res['status'] = 200
     res['msg'] = '请求成功'
