dca6134f43638d6d54750c504a26f2a944e6da88
GuangJun <3378882056@qq.com>
2020-12-23 18:36:01 +0800

    提供接口：根据ID查询公司信息

--- App/apis/corp_info.py
+++ App/apis/corp_info.py
@@ -29,7 +29,7 @@ def info():
         'introduction': 'I am a super administrator',
         'avatar': 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif'
     }
-    res = {} # 返回一个字典，包含状态码，信息，数据
+    res = {}  # 返回一个字典，包含状态码，信息，数据
     res['status'] = 200
     res['msg'] = '请求成功'
     res['data'] = data
@@ -38,24 +38,24 @@ def info():
 
 # 展示企业信息列表,每页展示10条
 @corp_info.route('/corp_info/<page>/<pre_page>', methods=['GET'])
-def corp_list(page,pre_page):
-    page=int(page)
-    pre_page=int(pre_page)
+def corp_list(page, pre_page):
+    page = int(page)
+    pre_page = int(pre_page)
 
-    total=CorpInfo.query.count()
-    start=(page-1)*pre_page
-    end=start+pre_page
-    pagination=Pagination(bs_version=3,page=page,total=total)
-    corp_list=CorpInfo.query.slice(start,end)
+    total = CorpInfo.query.count()
+    start = (page - 1) * pre_page
+    end = start + pre_page
+    pagination = Pagination(bs_version=3, page=page, total=total)
+    corp_list = CorpInfo.query.slice(start, end)
 
     items = []
     data = {}
     res = {}
     for corp in corp_list:
         tmp = corp.to_json()
-        time=str(tmp['establish_date'])
+        time = str(tmp['establish_date'])
         tmp = {
-            'id':tmp['id'],
+            'id': tmp['id'],
             'code': tmp['code'],
             'name': tmp['name'],
             'type': tmp['type'],
@@ -80,18 +80,18 @@ def corp_list(page,pre_page):
 # 根据企业名称查询企业信息列表,不用分页
 @corp_info.route('/corp_info/<corporation>', methods=['GET'])
 def corp_by_name(corporation):
-    total =1
-    corp_infos = CorpInfo.query.filter(CorpInfo.name==corporation)
+    total = 1
+    corp_infos = CorpInfo.query.filter(CorpInfo.name == corporation)
 
-    items=[]
+    items = []
     data = {}
     res = {}
 
     for corp_info in corp_infos:
         tmp = corp_info.to_json()
-        time=str(tmp['establish_date'])
+        time = str(tmp['establish_date'])
         tmp = {
-            'id':tmp['id'],
+            'id': tmp['id'],
             'code': tmp['code'],
             'name': tmp['name'],
             'type': tmp['type'],
@@ -103,32 +103,29 @@ def corp_by_name(corporation):
             'member': tmp['member']
         }
         items.append(tmp)
-    data['items']=items
-    data['total']=total
+    data['items'] = items
+    data['total'] = total
 
-    res['status']=200
-    res['msg']='请求成功'
-    res['data']=data
+    res['status'] = 200
+    res['msg'] = '请求成功'
+    res['data'] = data
     return jsonify(res)
 
 
 # 根据企业id查询企业信息,不用分页
 @corp_info.route('/corp_infoById/<id>', methods=['GET'])
 def corp_by_id(id):
-    id=int(id)
-    total =1
-    corp_infos = CorpInfo.query.filter(CorpInfo.id==id)
+    id = int(id)
+    total = 1
+    corp_infos = CorpInfo.query.filter(CorpInfo.id == id)
 
-    items=[]
     data = {}
     res = {}
-
     for corp_info in corp_infos:
         tmp = corp_info.to_json()
-        print(tmp)
-        time=str(tmp['establish_date'])
+        time = str(tmp['establish_date'])
         tmp = {
-            'id':tmp['id'],
+            'id': tmp['id'],
             'code': tmp['code'],
             'name': tmp['name'],
             'type': tmp['type'],
@@ -139,13 +136,11 @@ def corp_by_id(id):
             'businessScope': tmp['business_scope'],
             'member': tmp['member']
         }
-        items.append(tmp)
-    data['items']=items
-    data['total']=total
+        data['corpInfo'] = tmp
 
-    res['status']=200
-    res['msg']='请求成功'
-    res['data']=data
+    res['status'] = 200
+    res['msg'] = '请求成功'
+    res['data'] = data
     return jsonify(res)
 
 
